e10a88ae01962cae666be20d57331d1f2d3a652f
Zaqueu Souza <39178924+zaccsouza@users.noreply.github.com>
2020-12-11 22:47:10 -0300

    Re-Indentando o código

--- application/controllers/Cadastro.php
+++ application/controllers/Cadastro.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Cadastro extends CI_Controller{
     public function __construct(){
         parent:: __construct();
-        $this->load->model('Agente_model');//carregando o model do agente
+        $this->load->model('Agente_model'); //carregando o model do agente
     }
 
     public function index(){
