91ece59364dbe8fe7de957acc50486a244794b28
mateusmax076 <72174578+mateusmax076@users.noreply.github.com>
2020-12-30 12:33:37 -0300

    colocando nome da fabrica

--- application/views/administrador/ocorrencias-view-admin.php
+++ application/views/administrador/ocorrencias-view-admin.php
@@ -252,7 +252,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 2.4.18
     </div>
-    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE</a>.</strong> All rights
+    <strong>Copyright &copy; 2020 Fábrica de Software.</strong> All rights
     reserved.
   </footer>
 
